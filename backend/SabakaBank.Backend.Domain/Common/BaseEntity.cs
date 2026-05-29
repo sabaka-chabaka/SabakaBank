2db308ad364687291af7d9633fa606da6d53ec31
sabaka-chabaka <gamekimg360@gmail.com>
2026-05-29 23:55:19 +0400

    BaseEntity.cs

---
+++ backend/SabakaBank.Backend.Domain/Common/BaseEntity.cs
@@ -0,0 +1,10 @@
+namespace SabakaBank.Backend.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; protected set; } = Guid.NewGuid();
+    public DateTime CreatedAt { get; protected set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; protected set; } = DateTime.UtcNow;
+
+    protected void Touch() => UpdatedAt = DateTime.UtcNow;
+}
